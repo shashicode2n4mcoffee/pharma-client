8c48ba8c7f835c1dc72eb583c0891f968bd7f804
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-02-19 18:02:02 +0530

    profile query conditions added

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
-// import { Investigation } from '../../components/profile/investigation';
-// import { ProfileView } from '../../components/profile/view';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { Investigation } from '../../components/profile/investigation';
+import { ProfileView } from '../../components/profile/view';
 import { SidebarComponent } from '../../components/profile/sidebar';
 import { Treatment } from '../../components/profile/treatment';
-// import { PatientHistory } from '../../components/profile/patienthistory';
+import { PatientHistory } from '../../components/profile/patienthistory';
 
 export const Profile = () => {
+	const [searchParams] = useSearchParams();
+	const [profileLink, setProfileLink] = useState(null);
+
+	useEffect(() => {
+		setProfileLink(searchParams.get('profile'));
+	}, [searchParams]);
+
 	return (
 		<section className='h-2/3 flex justify-center items-start min-h-max pt-8'>
-			<SidebarComponent/>
-			{/* <ProfileView/> */}
-			{/* <PatientHistory/> */}
-			{/* <Investigation/> */}
-			<Treatment/>
+			<SidebarComponent />
+			{(profileLink === 'view' || profileLink === null) && <ProfileView />}
+			{profileLink === 'history' && <PatientHistory />}
+			{profileLink === 'investigation' && <Investigation />}
+			{profileLink === 'treatment' && <Treatment />}
 		</section>
 	);
 };
