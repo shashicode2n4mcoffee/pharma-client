8bb1e1fd310f44dbf3cbac605cba4fe621af22cb
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-02-19 13:38:54 +0530

    Navbar updated

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,51 +1,52 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { logoutUser, verifyUserDetails } from '../../store/auth/authActions';
-
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { NavLink } from 'react-router-dom';
+// import { logoutUser, verifyUserDetails } from '../../store/auth/authActions';
+import { Button, Navbar as NavbarComponent } from 'flowbite-react'
 
 const Navbar = () => {
-	const dispatch = useDispatch();
-	const { user, accessToken } = useSelector((state) => state.auth);
+	// const dispatch = useDispatch();
+	// const { user, accessToken } = useSelector((state) => state.auth);
 
-	useEffect(() => {
-		if (accessToken) {
-			dispatch(verifyUserDetails());
-		}
+	// useEffect(() => {
+	// 	if (accessToken) {
+	// 		dispatch(verifyUserDetails());
+	// 	}
 
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	// // eslint-disable-next-line react-hooks/exhaustive-deps
+	// }, []);
 
-	const logoutHandler = () => {
-		dispatch(logoutUser());
-	};
+	// const logoutHandler = () => {
+	// 	dispatch(logoutUser());
+	// };
 
 	return (
-	<header>
-		<div>
-			<span>
-				{user ? `Logged in as ${user.email}` : "You're not logged in"}
-			</span>
-			<div>
-				{user ? (
-					<NavLink onClick={logoutHandler}>
-						Logout
-					</NavLink>
-				) : (
-					<NavLink to='/login'>
-						Login
-					</NavLink>
-				)}
-			</div>
-		</div>
-		<nav>
-			<ul>
-				<li><NavLink to='/'>Home</NavLink></li>
-			</ul>
-		</nav>
-	</header>
-
-	);
+    <NavbarComponent fluid={true} rounded={true}>
+      <NavbarComponent.Brand href=''>
+        <img
+          src='https://flowbite.com/docs/images/logo.svg'
+          className='mr-3 h-6 sm:h-9'
+          alt='Flowbite Logo'
+        />
+        <span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
+          Patted Clinic
+        </span>
+      </NavbarComponent.Brand>
+      <div className='flex md:order-2'>
+        <Button>Login</Button>
+        <NavbarComponent.Toggle />
+      </div>
+      <NavbarComponent.Collapse>
+        <NavbarComponent.Link href='/navbars' active={true}>
+          Home
+        </NavbarComponent.Link>
+        <NavbarComponent.Link href='/navbars'>About</NavbarComponent.Link>
+        <NavbarComponent.Link href='/navbars'>Services</NavbarComponent.Link>
+        <NavbarComponent.Link href='/navbars'>Pricing</NavbarComponent.Link>
+        <NavbarComponent.Link href='/navbars'>Contact</NavbarComponent.Link>
+      </NavbarComponent.Collapse>
+    </NavbarComponent>
+  )
 };
 
 export { Navbar };
