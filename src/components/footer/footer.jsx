1221d55a1b3573c8efb082cd3c9b4f4b3a8e784d
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-02-19 20:27:20 +0530

    footer added

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import { Footer } from 'flowbite-react';
+
+export const FooterComponent = () => {
+	return (
+		<Footer className="w-screen">
+			<Footer.Copyright by='Shashikumar' year={2023} />
+			<Footer.LinkGroup>
+				<Footer.Link >About</Footer.Link>
+				<Footer.Link >Privacy Policy</Footer.Link>
+				<Footer.Link >Licensing</Footer.Link>
+				<Footer.Link >Contact</Footer.Link>
+			</Footer.LinkGroup>
+		</Footer>
+	);
+};
