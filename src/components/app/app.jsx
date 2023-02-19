bc17f0b5b2070659e835dc3ba31fc7509f9be52e
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-02-19 16:09:09 +0530

    profile route added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Dashboard } from '../../pages/dashboard';
 import { Home } from '../../pages/home';
 import { Login } from '../../pages/login';
+import { Profile } from '../../pages/profile';
 import { Register } from '../../pages/register';
 import { PrivateRoute } from '../privateRoute';
 import { PublicRoute } from '../publicRoute';
@@ -12,10 +13,23 @@ function App() {
 		<div>
 			<Layout>
 				<Routes>
-					<Route path="/" element={<PrivateRoute component={Home} />} />
-					<Route path='/login' element={<PublicRoute restricted={true} component={Login} />} />
-					<Route path='/register' element={<PublicRoute restricted={true} component={Register} />} />
-					<Route path="/dashboard" element={<PublicRoute restricted={true} component={Dashboard}/>}/>
+					<Route path='/' element={<PrivateRoute component={Home} />} />
+					<Route
+						path='/login'
+						element={<PublicRoute restricted={true} component={Login} />}
+					/>
+					<Route
+						path='/register'
+						element={<PublicRoute restricted={true} component={Register} />}
+					/>
+					<Route
+						path='/dashboard'
+						element={<PublicRoute restricted={true} component={Dashboard} />}
+					/>
+					<Route
+						path='/profile/:id'
+						element={<PublicRoute restricted={true} component={Profile} />}
+					/>
 				</Routes>
 			</Layout>
 		</div>
