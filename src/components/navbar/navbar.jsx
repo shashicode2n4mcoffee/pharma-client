178bf4881859951509706c3bea3eb1b4927a5813
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-02-19 13:39:10 +0530

    Navbar updated

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,7 +2,7 @@
 // import { useDispatch, useSelector } from 'react-redux';
 // import { NavLink } from 'react-router-dom';
 // import { logoutUser, verifyUserDetails } from '../../store/auth/authActions';
-import { Button, Navbar as NavbarComponent } from 'flowbite-react'
+import { Button, Navbar as NavbarComponent } from 'flowbite-react';
 
 const Navbar = () => {
 	// const dispatch = useDispatch();
@@ -21,32 +21,32 @@ const Navbar = () => {
 	// };
 
 	return (
-    <NavbarComponent fluid={true} rounded={true}>
-      <NavbarComponent.Brand href=''>
-        <img
-          src='https://flowbite.com/docs/images/logo.svg'
-          className='mr-3 h-6 sm:h-9'
-          alt='Flowbite Logo'
-        />
-        <span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
+		<NavbarComponent fluid={true} rounded={true}>
+			<NavbarComponent.Brand href=''>
+				<img
+					src='https://flowbite.com/docs/images/logo.svg'
+					className='mr-3 h-6 sm:h-9'
+					alt='Flowbite Logo'
+				/>
+				<span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
           Patted Clinic
-        </span>
-      </NavbarComponent.Brand>
-      <div className='flex md:order-2'>
-        <Button>Login</Button>
-        <NavbarComponent.Toggle />
-      </div>
-      <NavbarComponent.Collapse>
-        <NavbarComponent.Link href='/navbars' active={true}>
+				</span>
+			</NavbarComponent.Brand>
+			<div className='flex md:order-2'>
+				<Button>Login</Button>
+				<NavbarComponent.Toggle />
+			</div>
+			<NavbarComponent.Collapse>
+				<NavbarComponent.Link href='/navbars' active={true}>
           Home
-        </NavbarComponent.Link>
-        <NavbarComponent.Link href='/navbars'>About</NavbarComponent.Link>
-        <NavbarComponent.Link href='/navbars'>Services</NavbarComponent.Link>
-        <NavbarComponent.Link href='/navbars'>Pricing</NavbarComponent.Link>
-        <NavbarComponent.Link href='/navbars'>Contact</NavbarComponent.Link>
-      </NavbarComponent.Collapse>
-    </NavbarComponent>
-  )
+				</NavbarComponent.Link>
+				<NavbarComponent.Link href='/navbars'>About</NavbarComponent.Link>
+				<NavbarComponent.Link href='/navbars'>Services</NavbarComponent.Link>
+				<NavbarComponent.Link href='/navbars'>Pricing</NavbarComponent.Link>
+				<NavbarComponent.Link href='/navbars'>Contact</NavbarComponent.Link>
+			</NavbarComponent.Collapse>
+		</NavbarComponent>
+	);
 };
 
 export { Navbar };
