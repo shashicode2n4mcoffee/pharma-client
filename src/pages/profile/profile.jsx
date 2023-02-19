41e4408120d423d24ed5ac332926960aa772503a
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-02-19 16:59:22 +0530

    investigation component added

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Investigation } from '../../components/profile/investigation';
 // import { ProfileView } from '../../components/profile/view';
 import { SidebarComponent } from '../../components/profile/sidebar';
-import { PatientHistory } from '../../components/profile/patienthistory';
+// import { PatientHistory } from '../../components/profile/patienthistory';
 
 export const Profile = () => {
 	return (
 		<section className='h-2/3 flex justify-center items-start min-h-max pt-8'>
 			<SidebarComponent/>
 			{/* <ProfileView/> */}
-			<PatientHistory/>
+			{/* <PatientHistory/> */}
+			<Investigation/>
 		</section>
 	);
 };
